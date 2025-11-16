24a97bd17bdbffe87bce699c0776c929f555d847
twbaty <51901395+twbaty@users.noreply.github.com>
2025-11-16 16:14:57 -0600

    Update schema.py

--- database/schema.py
+++ database/schema.py
@@ -1,1 +1,21 @@
+from sqlalchemy import (
+    MetaData, Table, Column,
+    String, Integer, DateTime, Float,
+    Text, JSON
+)
+
+metadata = MetaData()
+
+battles = Table(
+    "battles",
+    metadata,
+    Column("id", String, primary_key=True),     # Q-ID
+    Column("label", String),
+    Column("description", Text),
+    Column("start", String),
+    Column("end", String),
+    Column("latitude", Float),
+    Column("longitude", Float),
+    Column("raw_coordinates", JSON),
+)
 
